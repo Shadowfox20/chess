a1edff4da779a98e45ad0308984019a479ef0ce5
Robert Thompson <robertthompsoncalifornia@gmail.com>
2026-09-14 19:54:08 -0600

    Chess position basic

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -56,7 +56,7 @@ public class ChessPiece {
 
     @Override
     public boolean equals(Object obj) {
-        if (obj.getClass() == ChessPiece.class) {
+        if (obj == null || obj.getClass() == ChessPiece.class) {
             return this.hashCode() == obj.hashCode();
         }
         return false;
--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -7,8 +7,12 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessPosition {
+    private final int row;
+    private final int col;
 
     public ChessPosition(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 
     /**
@@ -16,7 +20,7 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return row;
     }
 
     /**
@@ -24,6 +28,20 @@ public class ChessPosition {
      * 1 codes for the left column
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return col;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * row + 79 * col;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || obj.getClass() != ChessPosition.class) {
+            return false;
+        }
+        ChessPosition other = (ChessPosition) obj;
+        return row == other.getRow() && col == other.getColumn();
     }
 }
